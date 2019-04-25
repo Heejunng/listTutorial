631b1e4924ce53431fdb6d52743c72bdb4bdd83b
Heejunng <qusgmlwjd25@naver.com>
2019-04-25 14:26:25 +0900

    update

--- app/src/main/java/com/example/listtutorial/MainActivity.java
+++ app/src/main/java/com/example/listtutorial/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.listtutorial;
 
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import android.content.Intent;
 
@@ -12,31 +14,24 @@ import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button 한식;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        한식 = (Button)findViewById (R.id.한식);
+        한식.setOnClickListener (new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this, hansik.class);
+                startActivity (intent);
+            }
+        });
     }
 
-    
 
-    public void onButton1Clicked(View v) {
-        Toast.makeText(this, "카페 목록", Toast.LENGTH_LONG).show();
-        //Toast를 이용한 문구 출력
-        Intent intent = new Intent(this, MenuActivity.class);
-        //다른 클래스로 전환하는 코드
-        startActivity(intent);
 
-    }
-
-    public void onButton2Clicked(View v) {
-        Toast.makeText(this, "식당 목록", Toast.LENGTH_LONG).show();
-        //Toast를 이용한 문구 출력
-        Intent intent = new Intent(this, Menu2Activity.class);
-        //다른 클래스로 전환하는 코드
-        startActivity(intent);
-    }
 
 }
---
+++ app/src/main/java/com/example/listtutorial/bunsik.java
@@ -0,0 +1,4 @@
+package com.example.listtutorial;
+
+public class bunsik {
+}
